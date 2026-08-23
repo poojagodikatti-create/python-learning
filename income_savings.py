9a539e9aed8597785932e2a0cc8511b8c6e33c60
Pooja D G <poojagodikatti@gmail.com>
2026-08-23 13:18:33 +0530

    Income Calculater

---
+++ income_savings.py
@@ -0,0 +1,8 @@
+income = int(input("Enter your monthly income:"))
+expenses = int(input("Enter your monthly expenses:"))
+
+print("Montly Income:", income)
+print("Monthly Expenses:", expenses)
+
+savings = income - expenses 
+print("Monthly Savings:", savings)
