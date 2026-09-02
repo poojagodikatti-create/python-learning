7000d55407977b8b301f68617810df2684dafdf9
Pooja D G <poojagodikatti@gmail.com>
2026-09-02 09:09:41 +0530

    Add python student list

---
+++ student_list.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Pooja", "marks": 85},
+    {"name": "Ankita", "marks": 90},
+    {"name": "Rahul", "marks": 75}
+]
+
+for student in students:
+    print(student["name"], "scored", student["marks"])
