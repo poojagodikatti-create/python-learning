c348b9d2a9669965c1cc33822f392895bf11cc23
Pooja D G <poojagodikatti@gmail.com>
2026-09-03 23:47:07 +0530

    Add python ASCII art generator

---
+++ ASCII_art_generator.py
@@ -0,0 +1,12 @@
+print("🌈 ASCII NAME ART")
+print("=" * 35)
+
+name = input("Enter your name: ").upper()
+
+print("\n✨ YOUR NAME ✨")
+print()
+
+for letter in name:
+    print("⭐ " + letter + " ⭐")
+
+print("\n" + "=" * 35)
