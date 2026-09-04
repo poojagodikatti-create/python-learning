f2fa96c49c4c25f536d0c34c7a60a907407a2a96
Pooja D G <poojagodikatti@gmail.com>
2026-09-04 23:35:27 +0530

    Add python calculate marks use functions

---
+++ cal_marks_function.py
@@ -0,0 +1,10 @@
+def students(name, marks):
+    print("Student:", name)
+    print("Total:",sum(marks))
+    print("Average:", (sum(marks)/500)*100)
+    print("Maximum:", max(marks))
+    print("min marks:", min(marks))
+students("pooja", [85, 78, 90, 66, 75])
+
+
+
