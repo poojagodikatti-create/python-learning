68fe71611d997df3900ec6e434ad567b3d3de426
Pooja D G <poojagodikatti@gmail.com>
2026-09-04 23:32:56 +0530

    Add python calculator use the functions

---
+++ calculater_function.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+def subtraction(a, b):
+    return a - b
+def multiple(a, b):
+    return a * b 
+def division(a, b):
+    return a / b
+    
+addition = add(5, 5)
+sub = subtraction(6, 4)
+multi = multiple(10, 10)
+divi = division(10, 2)
+
+print(addition)
+print(sub)
+print(multi)
+print(divi)
